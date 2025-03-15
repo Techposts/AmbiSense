351ae6dfc62ee9a86040fd53c698029a49235345
Ravi Singh <ravi.singh543@gmail.com>
2025-03-15 14:05:26 +0530

    Update radar_manager.cpp

--- AmbiSense/radar_manager.cpp
+++ AmbiSense/radar_manager.cpp
@@ -6,8 +6,10 @@
 ld2410 radar;
 
 void setupRadar() {
+  Serial.println("Initializing radar sensor...");
   RADAR_SERIAL.begin(256000, SERIAL_8N1, RADAR_RX_PIN, RADAR_TX_PIN);
   radar.begin(RADAR_SERIAL);
+  Serial.println("Radar sensor initialized");
 }
 
 bool processRadarReading() {
@@ -18,10 +20,12 @@ bool processRadarReading() {
     if (currentDistance == 0) currentDistance = radar.stationaryTargetDistance();
 
     if (currentDistance < minDistance) currentDistance = minDistance;
-
+    
+    // Calculate the LED start position based on current distance
     int start_led = map(currentDistance, minDistance, maxDistance, 0, numLeds - movingLightSpan);
     start_led = constrain(start_led, 0, numLeds - movingLightSpan);
 
+    // Update the LEDs
     updateLEDs(start_led);
     return true;
   }
